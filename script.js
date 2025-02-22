17a048240ddaa33069e2fb5fefc8edd336b4aaca
Rou-Dou <mahoney_matthew@live.com>
2025-02-22 17:52:26 -0500

    added login screen

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("hamburger").addEventListener("click", () => {
     if (document.getElementById("dropdown").classList.contains("hidden")) {
-        document.getElementById("dropdown").classList.remove("hidden")
+        document.getElementById("dropdown").classList.remove("hidden");
     }
 
     else {
-        document.getElementById("dropdown").classList.add("hidden")
+        document.getElementById("dropdown").classList.add("hidden");
     }
-})
+});
