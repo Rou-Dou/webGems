b7cd6d13804df1797cccf87a735cc9a4e0c841d0
Rou-Dou <mahoney_matthew@live.com>
2025-04-01 23:25:57 -0400

    test

--- server.js
+++ server.js
@@ -6,9 +6,10 @@
     app.use(express.static("public"));
 
     app.get("/getplayer", (req, res) => {
-      getPlayer();
-      // res.header("Content-Type", 'application/json');
-      // res.json(json_file);
+      let player_json = getPlayer();
+      json_file = JSON.stringify(player_json)
+      res.header("Content-Type", 'application/json');
+      res.json(json_file);
 
     });
 
@@ -19,8 +20,6 @@
     function getPlayer() {
       const json_file = require("./players.json");
       let json_parsed = JSON.parse(json_file);
+      return player_json = json_parsed["Name"]
 
-      for (let player of json_parsed.Name) {
-        console.log(player)
-      }
     }
