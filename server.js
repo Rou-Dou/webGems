64c668974a9cd0242dd245fb5e0f0f654b51cf64
Rou-Dou <mahoney_matthew@live.com>
2025-04-01 23:21:58 -0400

    testing getPlayer API

--- server.js
+++ server.js
@@ -5,16 +5,22 @@
 
     app.use(express.static("public"));
 
-    app.get("/test", (req, res) => {
-      res.status(200)
-    });
+    app.get("/getplayer", (req, res) => {
+      getPlayer();
+      // res.header("Content-Type", 'application/json');
+      // res.json(json_file);
 
-    app.get("/players", (req, res) => {
-      const json_file = require("./players.json");
-      res.header("Content-Type", 'application/json');
-      res.json(json_file)
-    })
+    });
 
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
+
+    function getPlayer() {
+      const json_file = require("./players.json");
+      let json_parsed = JSON.parse(json_file);
+
+      for (let player of json_parsed.Name) {
+        console.log(player)
+      }
+    }
