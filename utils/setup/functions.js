cf751c201b19d612acf01a9994487fcc8ba6a4b4
Rou-Dou <mahoney_matthew@live.com>
2025-04-17 12:16:06 -0400

    added mysql npm package, test code with sql

--- utils/setup/functions.js
+++ utils/setup/functions.js
@@ -1,7 +1,20 @@
 import jsonData from "../../players.json" with { type: "json"}
+import { mysql } from "mysql";
 
 export function getPlayer() {
-    let numPlayers = Object.keys(jsonData.Name).length
-    const randNum = Math.round(Math.random() * numPlayers)
-    return jsonData.Name[randNum]
+    let connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Wgtfyghtf90)",
+        database: "testdb"
+    });
+
+    connection.connect();
+
+    connection.query('SELECT * FROM Players', function(error, results, fields) {
+        if (error) throw error;
+        console.log('Here is stuff', results)
+    });
+
+    connection.end();
 };
