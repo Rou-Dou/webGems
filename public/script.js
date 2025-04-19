229a6949ffb241d3f44fda5594a59f45478eac6c
Rou-Dou <mahoney_matthew@live.com>
2025-04-19 02:02:20 -0400

    testing api

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 let hamburgerMenu = document.getElementById("hamburger");
 let hamburgerDropdown = document.getElementById("dropdown");
+let playerName = document.getElementById("PlayerName");
+const url = "http://192.168.1.166:8080"
+const getPlayer = "/getplayer"
 const submitButton = document.getElementById("submitButton");
 
 hamburgerMenu.addEventListener("click", () => {
@@ -12,14 +15,14 @@ hamburgerMenu.addEventListener("click", () => {
     }
 });
 
+document.onload(() => {
+    playerName.innerText = fetch(`${url}${getPlayer}`);
+});
+
 submitButton.addEventListener('click', (e) =>{
     let userText = document.getElementById("guess");
     const guess = userText.innerText
     console.log(userText)
     userText.innerText = "";
-})
+});
 
-// async function makeGuess(guess) {
-//     const https://
-//     await fetch()
-// }
