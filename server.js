4dac69f4fea44c55eedf1d196d436e7addd9b6f8
Rou-Dou <mahoney_matthew@live.com>
2025-04-28 18:18:20 -0400

    added new code to process session token from api call and remove the colon and white space

--- server.js
+++ server.js
@@ -12,22 +12,23 @@
     app.use(express.static("public"));
 
     app.get("/getplayer", async (req, res) => {
-      res.set("Access-Control-Allow-Origin", "*");
-      res.set("Access-Control-Allow-Methods", "GET");
       const response = await functions.getPlayer()
       console.log("this is a message: ", response);
+      
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Methods", "GET");
       res.set("Content-Type", "text/plain");
       res.send(response);
     });
 
     app.get("/getSessionToken", async (req, res) => {
-      res.set("Access-Control-Allow-Origin", "*");
-      res.set("Access-Control-Allow-Methods", "GET");
       const newSession = await functions.generateNewSession()
       .then((value) => {
         gameSessions.Sessions.push(value);
         console.log("New Session: ", value);
         console.log(gameSessions);
+        res.set("Access-Control-Allow-Origin", "*");
+        res.set("Access-Control-Allow-Methods", "GET");
         res.set("Content-Type", "text/plain");
         res.send(value.sessionID);
         console.log("Session ID: ", value.sessionID);
@@ -35,10 +36,10 @@
     });
 
     app.get("/getSessionInfo:token", async (req, res) => {
-      res.set("Access-Control-Allow-Methods", "GET");
-      res.set("Content-Type", "application/json");
-      console.log("params: ", req.params.token)
-      const sessionInfo = functions.searchSession(gameSessions.Sessions, req.params.token);
+      console.log("params: ", req.params.token.replace(":","").trim())
+      const token = req.params.token.replace(":","").trim();
+
+      const sessionInfo = functions.searchSession(gameSessions.Sessions, token);
       console.log("the session info: ", sessionInfo)
       let returnInfo = sessionInfo.then((value) => {
         console.log("This is the session info value: ", value)
@@ -46,6 +47,8 @@
       })
       console.log('returned info: ', returnInfo)
       res.set("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Methods", "GET");
+      res.set("Content-Type", "application/json");
       res.send(returnInfo);
       console.log("parsed info: ", returnInfo)
     }); 
