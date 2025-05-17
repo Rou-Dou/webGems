64bec0e5e87b7d35b35f72f2f505d22d39009340
unknown <mahoney_matthew@live.com>
2025-05-17 00:02:16 -0400

    fixed issue with script related to pathing

--- scripts/database.ps1
+++ scripts/database.ps1
@@ -1,11 +1,14 @@
 Remove-Item -Path "C:\ProgramData\MySQL\MySQL Server 8.4\Uploads\*" -Recurse -Force
-cd ..\sql
-Copy-Item -Path ".\Uploads\*" -Destination "C:\ProgramData\MySQL\MySQL Server 8.4\Uploads\"
-Copy-Item -Path ".\*.sql" -Destination "C:\ProgramData\webGems\
+
+if (!(Test-Path -Path "C:\ProgramData\webGems")) {
+	New-Item -ItemType Directory -Path "C:\ProgramData\webGems"
+	Write-Host "Directory created"
+}
+
+Copy-Item -Path "..\sql\Uploads\*" -Destination "C:\ProgramData\MySQL\MySQL Server 8.4\Uploads\"
+Copy-Item -Path "..\sql\Queries\*" -Destination "C:\ProgramData\webGems\"
 
 $user = "root"
-
-cd ..\; cd scripts;
 
 Get-Content test.sql | & mysql -u $user -p"Wgtfyghtf90)"
 
